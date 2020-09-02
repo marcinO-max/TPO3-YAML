d6d8a3c6c09d4ed804a1d316252fd35a96d7801e
marcinO-max <61971483+marcinO-max@users.noreply.github.com>
2020-09-02 13:06:37 +0200

    initial commit

---
+++ TPO3_OM_S16425/src/zad1/Tools.java
@@ -0,0 +1,46 @@
+/**
+ *
+ *  @author Ossowski Marcin S16425
+ *
+ */
+
+package zad1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.naming.directory.InvalidAttributeIdentifierException;
+
+import org.yaml.snakeyaml.Yaml;
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+import com.sun.swing.internal.plaf.metal.resources.metal_zh_TW;
+
+
+public class Tools {
+	
+	
+	
+	Map<String, List<String>> map = new LinkedHashMap<>();
+
+	public static Options createOptionsFromYaml(String fileName) throws Exception{
+
+			
+			Yaml yaml = new Yaml();
+			FileInputStream fileInputStream = new FileInputStream(fileName);
+			
+			Map <String, Object> map =  yaml.load(fileInputStream);
+			Options options = new Options(String.valueOf(map.get("host")), (int)map.get("port"), (boolean)map.get("concurMode"), (boolean)map.get("showSendRes"), (Map<String, List<String>>)(map.get("clientsMap")));
+			
+		
+		
+		
+		return options;
+	}
+}
